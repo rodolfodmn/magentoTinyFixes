63c83cad28eb3b9bd324809d4cba819bc6c16f5e
Rodolfo Domingos <rodolfo.domingos@bistwobis.com.br>
2019-02-20 14:23:01 -0300

    add fix do label color que fica sem as vezes

---
+++ fixColorLabel.php
@@ -0,0 +1,18 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+ini_set('xdebug.var_display_max_depth', -1);
+ini_set('xdebug.var_display_max_children', -1);
+ini_set('xdebug.var_display_max_data', -1);
+include 'app/Mage.php';
+Mage::app();
+$data =  Mage::helper('gtd')->product()->attribute()->getAttributes();
+foreach ($data as $key => $value) {
+    if ( $value->getAttributeCode() == "color") {
+        if (empty($value->getFrontendLabel())) {
+            $value->setFrontendLabel("Cor");
+            $value->save();
+        }
+    }
+}
